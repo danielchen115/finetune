11c8078f7aebd473fa29cd65ab564e756a3aa945
Daniel Chen <daniel.chen115@gmail.com>
2019-02-16 10:51:27 -0500

    setup connection with spotipy

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import spotipy
+import spotipy.util as util
+
+scope = 'user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-cur' \
+        'rently-playing user-read-playback-state user-top-read user-read-recently-played app-remote-control streaming' \
+        ' user-read-private user-library-read user-library-modify'
+
+os.environ["SPOTIPY_CLIENT_ID"] = 'e4fecf947d534799b152a18023bae635'
+os.environ["SPOTIPY_CLIENT_SECRET"] = '85c54c906e6b4695858061f2291f9838'
+os.environ["SPOTIPY_REDIRECT_URI"] = 'https://httpbin.org/get'
+
+token = util.prompt_for_user_token("user", scope)
+print(token)
