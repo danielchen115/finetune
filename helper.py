72b7152c414e3a3e836d4863cf5e3d9e3b2cb800
Daniel Chen <daniel.chen115@gmail.com>
2019-02-20 01:02:03 -0500

    fix redirect uri

--- helper.py
+++ helper.py
@@ -9,7 +9,7 @@ scope = 'user-library-read playlist-read-private playlist-read-collaborative use
 
 os.environ["SPOTIPY_CLIENT_ID"] = 'e4fecf947d534799b152a18023bae635'
 os.environ["SPOTIPY_CLIENT_SECRET"] = '85c54c906e6b4695858061f2291f9838'
-os.environ["SPOTIPY_REDIRECT_URI"] = 'https://www.spotify-finetune.herokuapp.com'
+os.environ["SPOTIPY_REDIRECT_URI"] = 'http://spotify-finetune.herokuapp.com'
 
 # token = util.prompt_for_user_token("user", scope)
 #
--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = 'e4fecf947d534799b152a18023bae635';
-var SPOTIFY_REDIRECT_URI = 'https://www.spotify-finetune.herokuapp.com';
+var SPOTIFY_REDIRECT_URI = 'http://spotify-finetune.herokuapp.com';
